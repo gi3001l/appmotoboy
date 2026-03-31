b5f7be38d8a472593422ca095b0b1faf873586de
gi3001l <giihpekena7@gmail.com>
2026-03-31 11:37:08 -0300

    Initialize Express server with basic route

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require(“express”);
+const app = express();
+
+app.get(“/“, (req, res) =| {
+  res.send(“Servidor rodando 🚀”);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(Servidor rodando na porta ${PORT});
+});
